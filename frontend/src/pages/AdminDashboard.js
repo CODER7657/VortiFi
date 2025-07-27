46d4db9cb88558e7799c4a4e22187ad32a304648
CODER7657 <119109496+CODER7657@users.noreply.github.com>
2025-07-27 17:00:06 +0530

    Update AdminDashboard.js

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { contractABI, contractAddress } from "../config";
-import "./AdminDashboard.css"; // ✅ Import the styling
+import "./AdminDashboard.css"; 
 
 const AdminDashboard = () => {
   const [candidateName, setCandidateName] = useState("");
